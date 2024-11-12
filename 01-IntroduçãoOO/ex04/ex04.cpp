27d663a7e0fb86e371ed68b1d560078a1da7125d
Victor A. P. Oliveira <victorandreoliveira@gmail.com>
2024-11-12 17:44:31 -0300

    Ajuste no ex04

--- 01-IntroduçãoOO/ex04/ex04.cpp
+++ 01-IntroduçãoOO/ex04/ex04.cpp
@@ -2,38 +2,9 @@
 #include <string>
 #include <format>
 
-using std::cout, std::endl, std::string;
-using std::format;
+#include "Carro.hpp"
 
-class Carro {
-public:
-	Carro(string marca, string modelo)
-	{		
-		setMarca(marca);
-		setModelo(modelo);
-	}
-	
-	string getMarca() {
-		return m_marca;
-	}
-	
-	void setMarca(string marca){
-		m_marca = ( marca.empty() ? "\"Sem marca\"" : marca );
-	}
-		
-	string getModelo() {
-		return m_modelo;
-	}
-	
-	void setModelo(string modelo){
-		m_modelo = ( modelo.empty() ? "\"Sem modelo\"" : modelo );;
-	}
-	
-private:
-	string m_marca;
-	string m_modelo;
-};
-
+using std::cout, std::format;
 
 int main() {
 	Carro possante{"Renault", "Duster"};
